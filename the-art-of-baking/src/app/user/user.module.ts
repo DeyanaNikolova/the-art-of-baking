3b0af29308806dd3bd0fe8c05566449268cddd84
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-07-27 18:28:55 +0100

    Generate profile component

--- the-art-of-baking/src/app/user/user.module.ts
+++ the-art-of-baking/src/app/user/user.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule
