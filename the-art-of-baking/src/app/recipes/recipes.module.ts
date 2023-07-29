032c5dfa8c4cfb005b0a25d9f7c47de9cd45eacc
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-07-29 12:33:37 +0100

    Generate recipe-details component

--- the-art-of-baking/src/app/recipes/recipes.module.ts
+++ the-art-of-baking/src/app/recipes/recipes.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
 
 
 @NgModule({
   declarations: [
-    AddRecipeComponent
+    AddRecipeComponent,
+    RecipeDetailsComponent
   ],
   imports: [
     CommonModule
