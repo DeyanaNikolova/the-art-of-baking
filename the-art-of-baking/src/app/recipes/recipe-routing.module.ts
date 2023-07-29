b906854250b3b34f619535fde185fc002fc5d92d
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-07-29 13:01:37 +0100

    Generate all-recipes module

---
+++ the-art-of-baking/src/app/recipes/recipe-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AllRecipesComponent } from './all-recipes/all-recipes.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
+
+
+const routes: Routes = [
+    {
+        path: 'recipes',
+        children: [
+            {
+                path: 'all-recipes',
+                component: AllRecipesComponent
+            },
+            {
+                path: ':recipeId',
+                component: RecipeDetailsComponent
+            }
+        ]
+    },
+    {
+        path: 'add-recipe',
+        component: AddRecipeComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class RecipeRoutingModule { }
