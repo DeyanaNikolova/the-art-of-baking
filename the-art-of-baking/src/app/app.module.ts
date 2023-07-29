16458c51a7073147bcd207154679ea8941c679d2
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-07-29 11:51:54 +0100

    Add user module

--- the-art-of-baking/src/app/app.module.ts
+++ the-art-of-baking/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { MainComponent } from './main/main.component';
 import { HomeComponent } from './home/home.component';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     CoreModule,
+    UserModule,
     AppRoutingModule,
   ],
   providers: [],
