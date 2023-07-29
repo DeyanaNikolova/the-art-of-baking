0b75b959e50b4cd135b4e2eb59b6279ec7130fd6
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-07-29 13:02:01 +0100

    Update recipes.module.ts

--- the-art-of-baking/src/app/recipes/recipes.module.ts
+++ the-art-of-baking/src/app/recipes/recipes.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddRecipeComponent } from './add-recipe/add-recipe.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import { AllRecipesComponent } from './all-recipes/all-recipes.component';
+import { RecipeRoutingModule } from './recipe-routing.module';
 
 
 
 @NgModule({
   declarations: [
     AddRecipeComponent,
-    RecipeDetailsComponent
+    RecipeDetailsComponent,
+    AllRecipesComponent
   ],
   imports: [
-    CommonModule
-  ],
-  exports: [AddRecipeComponent]
+    CommonModule, RecipeRoutingModule
+  ]
 })
 export class RecipesModule { }
