7d4fdaacf7f5531cf3a29065b0d75e5040ab3135
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-07-29 11:52:39 +0100

    Check isLoggedIn

--- the-art-of-baking/src/app/core/header/header.component.ts
+++ the-art-of-baking/src/app/core/header/header.component.ts
@@ -7,6 +7,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+isLoggedIn: boolean = true;
   constructor(private router: Router){}
 }
