4ccd1a835046c0c5f2b5cc4536d8f377f65f9218
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-08-07 18:57:02 +0100

    Add auth guard to login and profile

--- the-art-of-baking/src/app/core/header/header.component.ts
+++ the-art-of-baking/src/app/core/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-isLoggedIn: boolean = false;
-  constructor(private router: Router){}
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+    ){}
+
+  get isLoggedIn(): boolean{
+    return this.userService.isLogged;
+  }
+
+  get username(): string{
+    return this.userService.user?.username || '';
+  }
+
+  logout():void{
+    this.userService.logout();
+    this.router.navigate(['/']);
+  }
 }
