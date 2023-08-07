c8df32186fa08fbe144a4032bfedbc994a461166
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-08-07 15:16:18 +0100

    Update user-routing

--- the-art-of-baking/src/app/recipes/recipes.module.ts
+++ the-art-of-baking/src/app/recipes/recipes.module.ts
@@ -4,6 +4,7 @@ import { AddRecipeComponent } from './add-recipe/add-recipe.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 import { AllRecipesComponent } from './all-recipes/all-recipes.component';
 import { RecipeRoutingModule } from './recipe-routing.module';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -14,7 +15,9 @@ import { RecipeRoutingModule } from './recipe-routing.module';
     AllRecipesComponent
   ],
   imports: [
-    CommonModule, RecipeRoutingModule
+    CommonModule, 
+    RecipeRoutingModule,
+    RouterModule
   ]
 })
 export class RecipesModule { }
