bf2cfb06ca937ae6568eccff67d44fc89b5896e3
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-08-07 15:57:03 +0100

    Update user.service.ts

--- the-art-of-baking/src/app/user/user.service.ts
+++ the-art-of-baking/src/app/user/user.service.ts
@@ -1,23 +1,36 @@
 import { Injectable } from '@angular/core';
 import { User } from '../types/user';
 
-const USER_KEY = '[user]'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 user: User | undefined;
+USER_KEY = '[user]'
 
 get isLogged(): boolean{
   return !!this.user;
 }
   constructor() {
     try {
-     const lsUser = localStorage.getItem(USER_KEY) || "";
+     const lsUser = localStorage.getItem(this.USER_KEY) || "";
      this.user = JSON.parse(lsUser);
     } catch (error) {
       this.user = undefined;
     }
+   }
+
+   login(): void{
+this.user = {
+  email: 'peter@abv.bg',
+  username: 'peter'
+}
+localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+   }
+
+   logout():void{
+    this.user = undefined;
+    localStorage.removeItem(this.USER_KEY)
    }
 }
