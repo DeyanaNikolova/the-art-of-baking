6acc2ea724d9136d51e3106de3c1609cf972220d
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-08-07 15:17:02 +0100

    Generate user service

---
+++ the-art-of-baking/src/app/user/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { User } from '../types/user';
+
+const USER_KEY = '[user]'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+user: User | undefined;
+
+get isLogged(): boolean{
+  return !!this.user;
+}
+  constructor() {
+    try {
+     const lsUser = localStorage.getItem(USER_KEY) || "";
+     this.user = JSON.parse(lsUser);
+    } catch (error) {
+      this.user = undefined;
+    }
+   }
+}
