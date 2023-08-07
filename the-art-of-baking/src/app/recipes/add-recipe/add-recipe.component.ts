1964265330ebe2c69a07bf7ccb6924bf47a3a408
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-08-07 19:41:48 +0100

    Update add recipe component

--- the-art-of-baking/src/app/recipes/add-recipe/add-recipe.component.ts
+++ the-art-of-baking/src/app/recipes/add-recipe/add-recipe.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-add-recipe',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-recipe.component.css']
 })
 export class AddRecipeComponent {
+
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    ){}
 
 }
