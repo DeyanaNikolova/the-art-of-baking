fb2e15d66706af5753c319df9fc9cf80a1abcc4b
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-08-09 16:47:30 +0100

    Add validation to login form

--- the-art-of-baking/src/app/user/user.module.ts
+++ the-art-of-baking/src/app/user/user.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ValidateEqualModule } from 'ng-validate-equal';
+
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -18,8 +18,7 @@ import { UserRoutingModule } from './user-routing.module';
   imports: [
     CommonModule,
     UserRoutingModule,
-    FormsModule,
-    // ValidateEqualModule
+    FormsModule,  
   ]
 })
 export class UserModule { }
