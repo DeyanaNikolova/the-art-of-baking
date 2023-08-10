0e6db5a0414f5e1ca22dea92a63c737e63ff4107
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-08-10 17:25:43 +0100

    Add user profile

---
+++ the-art-of-baking/src/app/types/profile.ts
@@ -0,0 +1,7 @@
+import { User } from "./user";
+
+export interface Profile {
+username: string;
+email: string;
+//_id: User;
+}
--- the-art-of-baking/src/app/user/profile/profile.component.ts
+++ the-art-of-baking/src/app/user/profile/profile.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { Profile } from 'src/app/types/profile';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
 
+  profileData: Profile = {
+    username: '',
+    email: '',
+  }
+
+  constructor(private userService: UserService){}
+
+  ngOnInit(): void {
+    const { username, email } = this.userService.user!;
+    this.profileData = { username, email };
+  }
 }
