2e661ee62e1f0e1be3eb68a9e498c185a40e3004
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-10 17:49:49 +0000

    Remove interceptor

--- the-art-of-baking/src/app/app.interceptor.ts
+++ the-art-of-baking/src/app/app.interceptor.ts
@@ -1,55 +1,47 @@
-import { Injectable, Provider } from '@angular/core';
-import {
-  HTTP_INTERCEPTORS,
-  HttpEvent,
-  HttpHandler,
-  HttpInterceptor,
-  HttpRequest,
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
-import { UserService } from './user/user.service';
+// import { Injectable, Provider } from '@angular/core';
+// import {
+//   HTTP_INTERCEPTORS,
+//   HttpEvent,
+//   HttpHandler,
+//   HttpInterceptor,
+//   HttpRequest,
+// } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { environment } from 'src/environments/environment.development';
+// import { UserService } from './user/user.service';
+// import { Router } from '@angular/router';
 
-const { apiUrl } = environment;
-@Injectable()
-export class AppInterceptor implements HttpInterceptor {
-  constructor(private userService: UserService) {}
+// const { apiUrl } = environment;
+// @Injectable()
+// export class AppInterceptor implements HttpInterceptor {
+//   constructor(private userService: UserService, private router: Router) {}
 
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    const token = this.userService.getToken();
-console.log(token);
+//   intercept(
+//     req: HttpRequest<any>,
+//     next: HttpHandler
+//   ): Observable<HttpEvent<any>> {
+//     //  const token = this.userService.getToken();
 
-    if (req.url.startsWith('/users')) {
-      req = req.clone({
-        url: req.url.replace('/login', apiUrl),
-      });
-      if (token) {
-        req = req.clone({
-          headers: req.headers.set('X-Authorization', token),
-          withCredentials: true,
-        });
-      }
-    } else if (req.url.startsWith('/users')) {
-      req = req.clone({
-        url: req.url.replace('/register', apiUrl),
-      });
-      if (token) {
-        req = req.clone({
-          headers: req.headers.set('X-Authorization', token),
-          withCredentials: true,
-        });
-      }
-    }
 
-    return next.handle(req);
-  }
-}
+//     if (req.url.startsWith('/')) {
+//       req = req.clone({
+//         url: req.url.replace('/', apiUrl),
+//         withCredentials: true,
+//       });
+//     //  if (token) {
+//     //     req = req.clone({
+//     //       headers: req.headers.set('X-Authorization', token),
+//     //       withCredentials: true,
+//     //     });
+//     //   }
+//     } 
 
-export const appInterceptorProvider: Provider = {
-  multi: true,
-  useClass: AppInterceptor,
-  provide: HTTP_INTERCEPTORS,
-};
+//     return next.handle(req);
+//   }
+// }
+
+// export const appInterceptorProvider: Provider = {
+//   multi: true,
+//   useClass: AppInterceptor,
+//   provide: HTTP_INTERCEPTORS,
+// };
