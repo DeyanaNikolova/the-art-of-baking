813d6e1a77c3a0a1fc2b010b0b9c896befbf0f7e
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-03-10 17:51:41 +0000

    Update recipe.service.ts

--- the-art-of-baking/src/app/recipes/recipe.service.ts
+++ the-art-of-baking/src/app/recipes/recipe.service.ts
@@ -1,26 +1,52 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Recipe } from '../types/recipe';
 import { environment } from 'src/environments/environment.development';
+import { UserService } from '../user/user.service';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private userService: UserService) {}
 
   getRecipes() {
-    const { apiUrl } = environment
+    const { apiUrl } = environment;
     return this.http.get<Recipe[]>(`${apiUrl}/data/recipes`);
   }
 
-  getRecipe(id: string){
-    const { apiUrl } = environment
-    return this.http.get<Recipe>(`${apiUrl}/data/recipes/${id}`)
+  getRecipe(id: string) {
+    const { apiUrl } = environment;
+    return this.http.get<Recipe>(`${apiUrl}/data/recipes/${id}`);
   }
 
-  addRecipe(recipeName: string, shortDescription: string, imageUrl: URL,ingredients: string, instructions: string, prepTime: string, cookTime: string, servings: number){
-    const { apiUrl } = environment
-    return this.http.post<Recipe>(`${apiUrl}/data/recipes`, {recipeName, shortDescription, imageUrl, ingredients, instructions, prepTime, cookTime, servings});
+  addRecipe(
+    recipeName: string,
+    shortDescription: string,
+    imageUrl: URL,
+    ingredients: string,
+    instructions: string,
+    prepTime: string,
+    cookTime: string,
+    servings: number
+  ) {
+    const { apiUrl } = environment;
+    
+    const recipe = {
+      recipeName,
+      shortDescription,
+      imageUrl,
+      ingredients,
+      instructions,
+      prepTime,
+      cookTime,
+      servings,
+    };
+    const token = this.userService.getToken();
+    console.log(token);
+    const headers = new HttpHeaders({'Content-type': 'application/json', 'X-Authorization': token});
+    const options = {headers: headers};
+    return this.http.post<Recipe>(`${apiUrl}/data/recipes`,  recipe, options);
   }
 }
